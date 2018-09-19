ae112df8663b7ccb0eeee7e4f1c2d7e3488fd5bc
Jia Ni <dn1231@gmail.com>
2018-09-19 16:48:50 -0400

    completed critical path existing user test

--- page_models/page-object.js
+++ page_models/page-object.js
@@ -10,15 +10,49 @@ export default class Test {
       this.printedDressInstock = Selector('#center_column')
       this.printedDress = Xpath('//*[@id="center_column"]/ul/li[2]/div/div[2]/h5/a') // this is using relative xpath and later can be changed to dynamic xpath
       this.quantity = Selector('#quantity_wanted')
-      this.size = Selector('#group_1')
+      this.size = Selector('#group_1') //this can be changed to randomly add size
       this.pinkColor = Selector('#color_24')
       this.addToCart = Selector('#add_to_cart')
-
-    /*  <a class="product-name" href="http://automationpractice.com/index.php?id_product=4&amp;controller=product&amp;search_query=dress&amp;results=7" title="Printed Dress" itemprop="url">
-							Printed Dress
-						</a>
-            //*[@id="center_column"]/ul/li[2]/div/div[2]/h5/a
-      #center_column > ul > li:nth-child(2) > div > div.right-block > h5 > a
-      #center_column > ul > li:nth-child(2) > div > div.right-block > span */
+      this.checkout = Selector('.btn.btn-default.button.button-medium')
+      this.skuColorSize = Selector('#product_4_44_0_0')
+      this.checkColorSize = Xpath('//*[@id="product_4_44_0_0"]/td[2]/small[2]/a') //later can write out logic to check
+      this.quantityCheck = Selector('.cart_quantity.text-center').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.priceCheck = Selector('.price').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.totalShipping = Selector('#total_shipping').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.totalPrice = Selector('#total_price').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.proceedToCheckout = Selector('.button.btn.btn-default.standard-checkout.button-medium')
+      this.createEmail = Selector('#email_create')
+      this.submitEmail = Selector('#SubmitCreate')
+      this.existingEmailError = Selector('#create_account_error').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.emailAddressInput = Selector('#email')
+      this.passwordInput = Selector('#passwd')
+      this.signInButton = Selector('#SubmitLogin')
+      this.sameBillingCheckbox = Selector('#uniform-addressesAreEquals').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.addComment = Selector('#ordermsg')
+      this.moveToShipping = Xpath('//*[@id="center_column"]/form/p/button')
+      this.agreementError = Selector('.fancybox-error').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
+      this.closeAgreementError = Selector('.fancybox-item.fancybox-close')
+      this.termsCheckbox = Selector('#cgv')
+      this.payByBankWire = Selector('.bankwire')
+      this.payByCheck = Selector('.cheque')
+      this.otherPayment = Selector('.button-exclusive.btn.btn-default')
+      this.confirmOrder = Xpath('//*[@id="cart_navigation"]/button')
+      this.orderComplete = Xpath('//*[@id="center_column"]/div/p/strong').addCustomDOMProperties({
+        innerHTML: el => el.innerHTML
+        });
     }
 }
--- test_scenarios/automation-exercise.js
+++ test_scenarios/automation-exercise.js
@@ -7,6 +7,14 @@ fixture `My fixture`
     .page `http://automationpractice.com/index.php`;
 
 test('Critical Path, Search, and New User', async t => {
+
+    await t
+
+
+});
+
+test('Critical Path, Search, and Existing User', async t => {
+
     await t
       .click(action.logo) //validates main logo is present
       .typeText(action.searchField, 'dress') //later can add a data-helper to randomize search
@@ -16,11 +24,31 @@ test('Critical Path, Search, and New User', async t => {
       .click(action.size)
       .click(action.pinkColor)
       .click(action.addToCart)
-
+      .click(action.checkout)
+      .expect(action.skuColorSize).ok()
+      .expect(action.checkColorSize).ok()
+      .expect(action.quantityCheck.innerHTML).contains('value="12"')
+      .expect(action.priceCheck.innerHTML).contains("$611.88")
+      .expect(action.totalShipping.innerHTML).contains("$2.00")
+      .expect(action.totalPrice.innerHTML).contains("$613.88")
+      .click (action.proceedToCheckout)
+      .typeText(action.createEmail, 'dn1231@gmail.com')
+      .click(action.submitEmail)
+      .expect(action.existingEmailError.innerHTML).contains("An account using this email address has already been registered. Please enter a valid password or request a new one. ")
+      .typeText(action.emailAddressInput, 'james.jones@gmail.com')
+      .typeText(action.passwordInput, 'LnHrThbzn4Zpkyn')
+      .click(action.signInButton)
+      .expect(action.sameBillingCheckbox.innerHTML).contains('<span class="checked"><input type="checkbox" name="same" id="addressesAreEquals" value="1" checked="checked"></span>')
+      .typeText(action.addComment, 'testing order')
+      .click(action.moveToShipping)
+      .click(action.proceedToCheckout)
+      .expect(action.agreementError.innerHTML).contains('You must agree to the terms of service before continuing.')
+      .click(action.closeAgreementError)
+      .click(action.termsCheckbox)
+      .click(action.proceedToCheckout)
+      .click(action.payByCheck)
+      .click(action.otherPayment)
+      .click(action.payByBankWire)
+      .click(action.confirmOrder)
+      .expect(action.orderComplete.innerHTML).contains('Your order on My Store is complete.')
 })
-
-test('Critical Path, Search, and Existing User', async t => {
-
-	await t
-
-});
