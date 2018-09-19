c6902265cf88c7022c2b16bb25c2b6b1724aefb6
Jia Ni <dn1231@gmail.com>
2018-09-18 20:00:11 -0400

    add to cart with color selection

--- page_models/page-object.js
+++ page_models/page-object.js
@@ -7,10 +7,12 @@ export default class Test {
       this.logo = Selector('.logo.img-responsive');
       this.searchField = Selector('#search_query_top');
       this.searchButton = Selector('.btn.btn-default.button-search');
-      this.searchResult = Selector('.lighter');
-      this.headerCount = Selector('.heading-counter');
       this.printedDressInstock = Selector('#center_column')
-      this.Dress = Xpath('//*[@id="center_column"]/ul/li[2]/div/div[2]/h5/a')
+      this.printedDress = Xpath('//*[@id="center_column"]/ul/li[2]/div/div[2]/h5/a') // this is using relative xpath and later can be changed to dynamic xpath
+      this.quantity = Selector('#quantity_wanted')
+      this.size = Selector('#group_1')
+      this.pinkColor = Selector('#color_24')
+      this.addToCart = Selector('#add_to_cart')
 
     /*  <a class="product-name" href="http://automationpractice.com/index.php?id_product=4&amp;controller=product&amp;search_query=dress&amp;results=7" title="Printed Dress" itemprop="url">
 							Printed Dress
--- test_scenarios/automation-exercise.js
+++ test_scenarios/automation-exercise.js
@@ -11,8 +11,11 @@ test('Critical Path, Search, and New User', async t => {
       .click(action.logo) //validates main logo is present
       .typeText(action.searchField, 'dress') //later can add a data-helper to randomize search
       .click(action.searchButton)
-      .click(action.Dress)
-
+      .click(action.printedDress)
+      .typeText(action.quantity, '2')
+      .click(action.size)
+      .click(action.pinkColor)
+      .click(action.addToCart)
 
 })
 
